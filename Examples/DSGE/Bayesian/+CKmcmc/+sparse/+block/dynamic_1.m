5990e44e6facbaaabe6c4cdaeb5985d01ed47e5b
Michael Creel <michael.creel@uab.es>
2024-05-15 16:07:59 +0200

    add TCN for DSGE

--- Examples/DSGE/Bayesian/+CKmcmc/+sparse/+block/dynamic_1.m
+++
@@ -1,6 +0,0 @@
-function [y, T] = dynamic_1(y, x, params, steady_state, sparse_rowval, sparse_colval, sparse_colptr, T)
-  y(17)=params(6)*y(6)+params(7)*x(1);
-  y(18)=params(8)*y(7)+params(9)*x(2);
-  T(1)=params(10)*exp(y(18));
-  y(20)=T(1);
-end
--- Examples/DSGE/Bayesian/+CKmcmc/+sparse/+block/static_3.m
+++
@@ -1,3 +0,0 @@
-function [y, T] = static_3(y, x, params, sparse_rowval, sparse_colval, sparse_colptr, T)
-  y(9)=params(10)*exp(y(7));
-end
